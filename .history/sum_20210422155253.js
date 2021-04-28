6160face3a4bbab892d573f14bdfab2e5d79d09d
CarstenHennig <62505648+CarstenHennig@users.noreply.github.com>
2021-04-28 11:36:26 +0200

    Delete .history directory

--- .history/sum_20210422155253.js
+++
@@ -1,9 +0,0 @@
-// Create a function sum which accepts any amount of numbers and return the sum of their addition
-
-const sum = (val1, val2) => {
-  return val1 + val2;
-};
-
-console.log(sum(1)); // -> 1
-console.log(sum(1, 15)); // -> 16
-console.log(sum(25, 25, 20)); // -> 70
--- .history/sum_20210422160543.js
+++
@@ -1,9 +0,0 @@
-// Create a function sum which accepts any amount of numbers and return the sum of their addition
-
-const sum = (...val) => {
-  let result = 0;
-};
-
-console.log(sum(1)); // -> 1
-console.log(sum(1, 15)); // -> 16
-console.log(sum(25, 25, 20)); // -> 70
